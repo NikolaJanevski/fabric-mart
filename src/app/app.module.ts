b1c7e89a373fb3607753330dcbc7d19857d32fe1
NikolaJanevski <njanevsk@mix.wvu.edu>
2022-01-22 21:38:02 -0500

    Added footer.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { TopNavBarComponent } from 'src/app/navigation/topnavbar.component';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './navigation/footer/footer.component';
 import { SecondTopNavBarComponent } from './navigation/second.topnavbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopNavBarComponent,
-    SecondTopNavBarComponent
+    SecondTopNavBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
