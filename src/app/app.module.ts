8e02a60dd1d4060695ea776d0829527c18691ad2
NikolaJanevski <njanevsk@mix.wvu.edu>
2022-01-24 15:51:44 -0500

    Added round category card.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { TopNavBarComponent } from 'src/app/navigation/topnavbar.component';
 
 import { AppComponent } from './app.component';
+import { CategoryCardComponent } from './categories/category.card.component';
 import { FooterComponent } from './navigation/footer/footer.component';
 import { SocialFooterComponent } from './navigation/footer/social.footer.component';
 import { SecondTopNavBarComponent } from './navigation/second.topnavbar.component';
@@ -13,7 +14,8 @@ import { SecondTopNavBarComponent } from './navigation/second.topnavbar.componen
     TopNavBarComponent,
     SecondTopNavBarComponent,
     FooterComponent,
-    SocialFooterComponent
+    SocialFooterComponent,
+    CategoryCardComponent
   ],
   imports: [
     BrowserModule
