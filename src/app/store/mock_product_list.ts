fc9ee358d8868bf7a14e8bbd7a95b0d47f13cf4b
NikolaJanevski <njanevsk@mix.wvu.edu>
2022-01-31 22:47:32 -0500

    Added mock product list and added a ProductItemModel class.

---
+++ src/app/store/mock_product_list.ts
@@ -0,0 +1,20 @@
+export const mock_product_list = [
+    {
+        img: "./assets/thumbnail1.jpeg",
+        price: 10,
+        sku: "DLB6594",
+        description: "Light Cream 100% Cotton Denim Twill - NY Designer - 58W"
+    },
+    {
+        img: "./assets/thumbnail2.jpeg",
+        price: 19.99,
+        sku: "MGD6706",
+        description: "Dark Shadow Gray/Black 100% Cotton Twill Jacketing - NY Designer - 60W"
+    },
+    {
+        img: "./assets/thumbnail3.jpeg",
+        price: 16.99,
+        sku: "DCD6611d",
+        description: "Fern Green 100% Cotton Twill Bottom Weight - NY Designer - 62W"
+    }
+];
