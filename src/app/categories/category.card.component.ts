218064fcfffaad80c46edea0d064a48e4e143928
NikolaJanevski <njanevsk@mix.wvu.edu>
2022-02-03 21:28:00 -0500

    Add syle binding for category cards

--- src/app/categories/category.card.component.ts
+++ src/app/categories/category.card.component.ts
@@ -8,10 +8,12 @@ import { Component, Input } from "@angular/core";
 export class CategoryCardComponent {
     @Input() title: string;
     @Input() link_text: string;
+    @Input() bg_color: string;
 
     constructor() {
         this.title = "Missing Title";
         this.link_text = "Missing link text";
+        this.bg_color = "black";
     }
 
 }
--- src/app/categories/category_list.ts
+++ src/app/categories/category_list.ts
@@ -1,26 +1,32 @@
 export const category_list = [
     {
         title: 'DESIGNER',
-        link_text: 'Designer Fabric'
+        link_text: 'Designer Fabric',
+        bg_color: '#e6e2e9'
     },
     {
         title: 'Sales & Clearance',
-        link_text: 'Sales / Clearance Fabric'
+        link_text: 'Sales / Clearance Fabric',
+        bg_color: '#cdc5d3'
     },
     {
         title: 'A-D',
-        link_text: 'Fabrics A-D'
+        link_text: 'Fabrics A-D',
+        bg_color: '#b4a9bc'
     },
     {
         title: 'E-O',
-        link_text: 'Fabrics E-O'
+        link_text: 'Fabrics E-O',
+        bg_color: '#9b8ca6'
     },
     {
         title: 'P-Z',
-        link_text: 'Fabrics P-Z'
+        link_text: 'Fabrics P-Z',
+        bg_color: '#826f90'
     },
     {
         title: 'Sewing Room',
-        link_text: 'Sewing Room'
+        link_text: 'Sewing Room',
+        bg_color: '#685973'
     }
 ]
